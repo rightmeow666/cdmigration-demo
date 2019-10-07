3676be81faf2780910143c18c1e246374c74d2e3
sudofluff <sudofluff@icloud.com>
2019-10-07 15:06:19 -0700

    Adds api for performBackgroundTask

--- cdmigration-demo/ActivePersistence/CoreDataManager.swift
+++ cdmigration-demo/ActivePersistence/CoreDataManager.swift
@@ -10,71 +10,75 @@ import Foundation
 import CoreData
 
 class CoreDataManager: NSObject {
-    static let shared = CoreDataManager()
-    
-    private(set) var persistentContainer: NSPersistentContainer
-    
-    private let env: Environment
-    
-    lazy var viewContext: NSManagedObjectContext = {
-        let context = self.persistentContainer.viewContext
-        context.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
-        context.automaticallyMergesChangesFromParent = true
-        context.shouldDeleteInaccessibleFaults = true
-        context.name = "\(self.env.rawValue) environment viewContext: \(context.hashValue)"
-        return context
-    }()
-    
-    lazy private var containerPath: URL? = {
-        return URL(string: "")
-    }()
-    
-    func newBackgroundContext() -> NSManagedObjectContext {
-        let context = self.persistentContainer.newBackgroundContext()
-        context.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
-        context.name = "\(self.env) environment backgroundContext: \(context.hashValue)"
-        return context
+  static let shared = CoreDataManager()
+  
+  private var persistentContainer: NSPersistentContainer
+  
+  private let env: Environment
+  
+  lazy var viewContext: NSManagedObjectContext = {
+    let context = self.persistentContainer.viewContext
+    context.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
+    context.automaticallyMergesChangesFromParent = true
+    context.shouldDeleteInaccessibleFaults = true
+    context.name = "\(self.env.rawValue) environment viewContext: \(context.hashValue)"
+    return context
+  }()
+  
+  lazy private var containerPath: URL? = {
+    return URL(string: "")
+  }()
+  
+  func newBackgroundContext() -> NSManagedObjectContext {
+    let context = self.persistentContainer.newBackgroundContext()
+    context.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
+    context.name = "\(self.env) environment backgroundContext: \(context.hashValue)"
+    return context
+  }
+  
+  func performBackgroundTask(block: @escaping (NSManagedObjectContext) -> Void) {
+    self.persistentContainer.performBackgroundTask(block)
+  }
+  
+  func loadPersistentContainer() {
+    let startTime = DispatchTime.now()
+    self.persistentContainer.loadPersistentStores { (description, error) in
+      let endTime = DispatchTime.now()
+      let timeInterval = Double(endTime.uptimeNanoseconds - startTime.uptimeNanoseconds) / 1000000000
+      if let err = error {
+        fatalError("Failed to load persistent container of type under \(self.env) environment: \(err)")
+      } else {
+        print("PersistentContainer loaded in \(timeInterval) seconds under \(self.env) environment")
+      }
     }
-    
-    func loadPersistentContainer() {
-        let startTime = DispatchTime.now()
-        self.persistentContainer.loadPersistentStores { (description, error) in
-            let endTime = DispatchTime.now()
-            let timeInterval = Double(endTime.uptimeNanoseconds - startTime.uptimeNanoseconds) / 1000000000
-            if let err = error {
-                fatalError("Failed to load persistent container of type under \(self.env) environment: \(err)")
-            } else {
-                print("PersistentContainer loaded in \(timeInterval) seconds under \(self.env) environment")
-            }
-        }
+  }
+  
+  /// Convenient helper method to save everything on the viewContext when user suddenly closes the app.
+  func saveViewContext() {
+    if self.viewContext.hasChanges {
+      do {
+        try self.viewContext.save()
+      } catch let err {
+        fatalError("Unresolved error \(err.localizedDescription)")
+      }
     }
-    
-    /// Convenient helper method to save everything on the viewContext when user suddenly closes the app.
-    func saveViewContext() {
-        if self.viewContext.hasChanges {
-            do {
-                try self.viewContext.save()
-            } catch let err {
-                fatalError("Unresolved error \(err.localizedDescription)")
-            }
-        }
+  }
+  
+  private init(_ env: Environment = .development) {
+    self.env = env
+    let container = NSPersistentContainer(name: "Model")
+    switch env {
+    case .development:
+      break
+    case .test:
+      let description = NSPersistentStoreDescription()
+      description.type = NSInMemoryStoreType
+      description.shouldAddStoreAsynchronously = false
+      container.persistentStoreDescriptions = [description]
+    case .production:
+      break
     }
-    
-    private init(_ env: Environment = .development) {
-        self.env = env
-        let container = NSPersistentContainer(name: "Model")
-        switch env {
-        case .development:
-            break
-        case .test:
-            let description = NSPersistentStoreDescription()
-            description.type = NSInMemoryStoreType
-            description.shouldAddStoreAsynchronously = false
-            container.persistentStoreDescriptions = [description]
-        case .production:
-            break
-        }
-        self.persistentContainer = container
-        super.init()
-    }
+    self.persistentContainer = container
+    super.init()
+  }
 }
--- cdmigration-demo/DB/Seeds.swift
+++ cdmigration-demo/DB/Seeds.swift
@@ -10,31 +10,31 @@ import Foundation
 import Fakery
 
 struct Seeds {
-    static func make(userCount: Int, postCountPerUser: Int) throws {
-        let faker = Faker(locale: "nb-NO")
-        
-        for _ in 0 ..< userCount {
-            CoreDataManager.shared.persistentContainer.performBackgroundTask { (moc) in
-                let u = User(context: moc)
-                u.save(moc, block: {
-                    u.email = faker.internet.email()
-                    u.id = faker.internet.password()
-                    u.name = faker.internet.username()
-                    for _ in 0 ..< postCountPerUser {
-                        let p = Post(context: moc)
-                        p.createdAt = Date()
-                        p.title = faker.lorem.word()
-                        p.updatedAt = Date()
-                    }
-                }) { (result) in
-                    switch result {
-                    case .failure(let err):
-                        print(err.localizedDescription)
-                    case .success:
-                        print("success")
-                    }
-                }
-            }
+  static func make(userCount: Int, postCountPerUser: Int) throws {
+    let faker = Faker(locale: "nb-NO")
+    
+    for _ in 0 ..< userCount {
+      CoreDataManager.shared.performBackgroundTask { (moc) in
+        let u = User(context: moc)
+        u.save(moc, block: {
+          u.email = faker.internet.email()
+          u.id = faker.internet.password()
+          u.name = faker.internet.username()
+          for _ in 0 ..< postCountPerUser {
+            let p = Post(context: moc)
+            p.createdAt = Date()
+            p.title = faker.lorem.word()
+            p.updatedAt = Date()
+          }
+        }) { (result) in
+          switch result {
+          case .failure(let err):
+            print(err.localizedDescription)
+          case .success:
+            print("success")
+          }
         }
+      }
     }
+  }
 }
